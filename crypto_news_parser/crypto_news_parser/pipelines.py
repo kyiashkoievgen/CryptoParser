ec91aaa3ecd5719753d8869ed4606fa50910ba7f
kyiashkoievgen <kyiashkoievgen@gmail.com>
2023-03-02 11:13:17 +0900

    binance_api remove 'add_url' from add date base

--- crypto_news_parser/crypto_news_parser/pipelines.py
+++ crypto_news_parser/crypto_news_parser/pipelines.py
@@ -16,15 +16,15 @@ class CryptoNewsParserPipeline:
         self.mongo_db = client.db_crypto_data
 
     def process_item(self, item, spider):
-
+        url = item.pop('add_url')
         if spider.name == 'binance_api':
-            if item['add_url'].find('depth') > 0:
+            if url.find('depth') > 0:
                 collection = self.mongo_db['depth']
                 collection.insert_one(item)
-            elif item['add_url'].find('trades') > 0:
+            elif url.find('trades') > 0:
                 collection = self.mongo_db['rades']
                 collection.insert_one(item)
-            elif item['add_url'].find('klines') > 0:
+            elif url.find('klines') > 0:
                 collection = self.mongo_db['klines']
                 collection.insert_one(item)
         else:
